77682f9a81bdce199932e4b8d8406507fd1c7db4
Dominic Summers <dominic@khord.co>
2020-01-21 15:44:29 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,16 +39,6 @@ const Footer = class extends React.Component {
                         Our Services
                       </Link>
                     </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
                   </ul>
                 </section>
               </div>
