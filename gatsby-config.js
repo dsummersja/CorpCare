d881417af9fdbe2db326df462ceaf6d26122d15d
Dominic Summers <dominic@khord.co>
2020-01-21 16:14:44 -0500

    Site title changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'CorpCare',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'CorpCare is a Corporate Social Responsibility (CSR) strategy and execution firm for businesses and philanthropists seeking to give and do more for their communities.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
