b85bc811511e0b1e1630ea01e760d91ea70f648e
Dominic Summers <dominic@khord.co>
2020-02-10 12:56:54 -0500

    visual changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="CorpCare" style={{ width: '216px', maxHeight: '5rem' }} />
+              <img src={logo} alt="CorpCare" style={{ width: '266px', maxHeight: '5rem' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -71,7 +71,7 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="#partners">
                 Our Partners
               </Link>
-              <Link className="navbar-item" to="#partners">
+              <Link className="navbar-item" to="/contact">
                 Contact Us
               </Link>
             </div>
