fcc5dfcc07d4ea7eb31b93a975b44bad1181760c
Dominic Summers <dominic@khord.co>
2020-06-03 17:57:06 -0500

    test resources page

---
+++ src/pages/resources.js
@@ -0,0 +1,151 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const ResourcePage = () => (
+  <Layout>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Resources</h1>
+        <h2>For donating to and volunteering with human rights and justice organisations who advocate for equal rights, access to opportunity and the betterment of our societies. Please note that this page is a “living” resource and will constantly be updated. </h2>
+       <br />
+        <div className="grid-container">
+          <div className="Local">
+            <h1>Jamaican Organisations</h1>
+            <br />
+            <ul>
+              <li>
+                <h1>876Volunteer</h1>
+                    876Volunteer is centered around “connecting good hearts to good causes”. Their website is accessible <a href="https://www.876volunteer.org">here</a>. Slide into their DMs on <a href=" https://www.instagram.com/876volunteer/">Instagram</a> to learn more about how you can volunteer in your community. CorpCare’s MD serves an ambassador for 876Volunteer.
+              </li>
+              <br />
+              <li>
+                <h1>Jamaicans For Justice (JFJ)</h1> 
+                CorpCare works closely with JFJ and recommends to its family of companies and its wider community to become members of the JFJ contributing. For those interested in volunteering with JFJ: <a href="https://www.jamaicansforjustice.org/volunteer/">Click Here</a>. For those interested in making their advocacy a full-time job, JFJ provides information about its job opportunities <a href="https://www.jamaicansforjustice.org/current-openings/">here</a>. To donate, contact the local JFJ office. 
+              </li>
+              <br />
+              <li>
+                <h1>Council of Voluntary Services (CVSS)</h1>
+                CVSS serves as an umbrella organisation for volunteer and advocacy organisations in Jamaica. Their website is accessible <a href="https://cvssja.org/">here</a>. A list of their member agencies can be found <a href="https://cvssja.org/about-us/member-agencies/">here</a> 
+              </li>
+            </ul>
+            <br />
+            <h1>Caribbean Organisations</h1>
+            <ul>
+              <li><h2>We are asking that our fellow Caribbean citizens share additional resources with us <a href="https://corpcare.com/#contact">here.</a> </h2></li>
+            </ul>
+          </div>
+          <div className="International">
+            <h1>International Organisations</h1>
+            <br />
+            <ul>
+              <li>
+                <h1>Black Lives Matter</h1>
+                <a href="https://secure.actblue.com/donate/ms_blm_homepage_2019">Donate</a>
+              </li>
+              <li>
+                <h1>National Association for the Advancement of Colored People</h1>
+                <a href="https://secure.actblue.com/donate/naacp-1">Donate</a>
+              </li>
+              <li>
+                <h1>NAACP Legal Defense Fund</h1>
+                <a href="https://www.naacpldf.org/">Donate</a>
+              </li>
+              <li>
+                <h1>Obama Foundation </h1>
+                <a href="https://www.obama.org/donate/">Donate</a>
+              </li>
+              <li>
+                <h1>Black Girls Code</h1>
+                <a href="https://www.paypal.com/donate/?token=-3iIlo5l6OVtNkSMZzWuGfykfACwnBDr0LmrNFI2onQ4RW2QAzDA7VuT4sYvoQdgpJ0lEG&country.x=US&locale.x=US">Donate</a>
+              </li>
+              <li>
+                <h1>Rock The Vote</h1>
+                <a href="https://www.rockthevote.org/about-rock-the-vote/donate/">Donate</a>
+              </li>
+              <li>
+                <h1>American Civil Liberties Union (ACLU)</h1>
+                <a href="https://www.naacpldf.org/news/civil-rights-leaders-call-for-national-day-of-mourning-demand-federal-action-to-reform-police-2/">Donate</a>
+              </li>
+              <li>
+                <h1>The Audre Lorde Project</h1>
+                <a href="https://alp.org/get-involved">Donate</a>
+              </li>
+              <li>
+                <h1>National Bail Out</h1>
+                <a href="https://secure.actblue.com/donate/freeblackmamas2020">Donate</a>
+              </li>
+              <li>
+                <h1>Communities United for Police Reform </h1>
+                <a href="https://northstarfund.org/give/donate/donate-to-communities-united-for-police-reform/">Donate</a>
+              </li>
+              <li>
+              <h1>For updates on petitions to sign related to causes you care about or are interested in, please visit:</h1>  <a href="https://opm.gov.jm/participate/jamaica-house-petition/">Jamaica House Petition</a> or <a href="https://www.change.org/">Change.org</a> 
+              </li>
+            </ul>
+          </div>
+          <div className="Booklist">
+            <h1>CorpCare’s team booklist for issues of colonialism, racial segregation, marginalisation and injustice </h1>
+            <ul>
+              <li>Between the World and Me by Ta-Nehisi Coates</li>
+              <li>When They Call You a Terrorist: A Black Lives Matter Memoir by Patrisse Khan-Cullors and Asha Bandele</li>
+              <li>Augustown by Kei Miller</li>
+              <li>A Brief History of Seven Killings by Marlon James</li>
+              <li>Racism without Racists by Eduardo Bonilla-Silva</li>
+              <li>So You Want to Talk About Race' by Ijeoma Oluo</li>
+              <li>The Book of Black Royalty by Ibrahim Konteh and Kwame Gayle</li>
+              <li>Marcus Garvey: Ultimate Collection of Speeches and Poems</li>
+              <li>The Underground Railroad by Colson Whitehead</li>
+              <li>Homegoing by Yaa Gyasi</li>
+              <li>We Should All Be Feminists by Chimamanda Ngozi Adichie </li>
+              <li>Americanah by Chimamanda Ngozi Adichie</li>
+              <li>A Mayor’s Life by David Dinkins</li>
+              <li>Mountains Beyond Mountains by Tracy Kidder</li>
+              <li>More Myself by Alicia Keys</li>
+              <li>Becoming by Michelle Obama</li>
+              <li>Dare to Lead by Brene Brown</li>
+              <li>Untamed by Glennon Doyle</li>
+              <li>Year of Yes by Shonda Rhimes</li>
+              <li>The New Jim Crow by Michelle Alexander</li>
+              <li>Ayiti by Roxanne Gay</li>
+              <li>The Underground Railroad by Colson Whitehead</li>
+              <li>Requiem for the American Dream by Noam Chomsky</li>
+              <li>Born a Crime by Trevor Noah</li>
+              <li>Negro with a Hat by Colin Grant</li>
+              <li>The Colour Purple by Alice Walker</li>
+              <li>Eric E. Williams Speaks: Essays on Colonialism and Independence; edited by Selwyn R. Cudjoe</li>
+              <li>White Fragility: Why It’s so Hard for White People to Talk About Racism by Robin Diangelo</li>
+              <li>How to be be and Antiracist by Ibram X Kendi</li>
+              <li>Me and White Supremacy by Layla F. Saad</li>
+              <li>The New Jim Crow: Mass Incarcerations in the Age of Colorblindness by Michelle Alexander</li>
+              <li>Stamped from the Beginning: The Definitive History of Racist Ideas in America by Ibram X. Kendi</li>
+              <li>The Divide: American Injustice in the Age of the Wealth Gap by Matt Taibbi</li>
+              <li>Feminism is for Everybody by Bell Hooks </li>
+              <li>Bad Feminist by Roxanne Gay</li>
+              <li>Sister Outsider by Audre Lorde</li>
+              <li>Women, Culture, and Politics by Angela Y. Davis</li>
+              <li>Dear Ijeawele: A Feminist Manifesto In Fifteen Suggestions by Chimamanda Ngozi Adichie</li>
+              <li>I Know Why The Caged Bird Sings by Maya Angelou</li>
+              <li>I am Malala by Malala Yousafzai</li>
+              <li>Unbought and Unbossed by Shirley Chisholm</li>
+              <li>My Life on the Road by Gloria Steinem</li>
+              <li>Salt by Nayyirah Waheed</li>
+              <li>Nejma by Nayyirah Waheed</li>
+            </ul>
+          </div>
+          <div className="Docs">
+            <h1>Documentaries, Movies, TV Shows and other Media </h1>
+            <ul>
+              <li>13th - Documentary by Ava Duvernay</li>
+              <li>Whose Streets? - Documentary by Sabaah Folayan</li>
+              <li>I Am Not Your Negro - Movie by Raoul Peck</li>
+              <li>The Hate You Give - Movie</li>
+              <li>Patriot Act with Hassan Minhaj - Netflix Series</li>
+              <li>Mrs. America</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+export default ResourcePage
